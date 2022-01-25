a0460f6086744672f3a7a5403d9e0d1b572bb34a
roshanadhikari48 <adhikariroshan157@gmail.com>
2022-01-25 21:19:20 +0545

    'message'

---
+++ contents/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from contents.models import *
+
+admin.site.register(Profile)
+admin.site.register(Focus)
+admin.site.register(TechnicalSkill)
+admin.site.register(ProfessionalSkill)
+admin.site.register(Education)
+admin.site.register(WorkExperience)
+admin.site.register(ProjectCategory)
+admin.site.register(Project)
+admin.site.register(ToolsAndTech)
+admin.site.register(ProjectImage)
